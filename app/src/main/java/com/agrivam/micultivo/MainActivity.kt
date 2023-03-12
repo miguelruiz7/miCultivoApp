edceaff6ba0d658ea246c923dd4b25cd3dc9f909
miguelruiz7 <miguelruizzamora7@gmail.com>
2023-03-12 16:49:51 -0600

    Modificación en la conectividad en datos moviles

--- app/src/main/java/com/agrivam/micultivo/MainActivity.kt
+++ app/src/main/java/com/agrivam/micultivo/MainActivity.kt
@@ -4,15 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.*
 import android.webkit.WebView
 import androidx.appcompat.app.AlertDialog
-import java.net.HttpURLConnection
-import java.net.URL
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         //Verificamos si cumple con la conectividad
         if (capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(
-                NetworkCapabilities.TRANSPORT_CELLULAR) && canAccessUrl(url))
+                NetworkCapabilities.TRANSPORT_CELLULAR) && (URLUtil.isValidUrl(url)))
         ) {
             // Se abre el vieweb
             webView.webViewClient = object : WebViewClient() {
@@ -66,18 +62,5 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-    // Verifica si podemos accesar a esa url antes de mostrarnolos
-    private fun canAccessUrl(urlString: String): Boolean {
-        return try {
-            val url = URL(urlString)
-            val connection = url.openConnection() as HttpURLConnection
-            connection.connectTimeout = 3000
-            connection.connect()
-            val responseCode = connection.responseCode
-            responseCode == HttpURLConnection.HTTP_OK
-        } catch (e: Exception) {
-            false
-        }
-    }
 
 }
