10ccadb703783eec50d2aceee5e61c27fd921d57
miguelruiz7 <miguelruizzamora7@gmail.com>
2023-03-12 20:27:30 -0600

    Modificación para que no se viera feo el viewweb

--- app/src/main/java/com/agrivam/micultivo/MainActivity.kt
+++ app/src/main/java/com/agrivam/micultivo/MainActivity.kt
@@ -2,12 +2,16 @@ package com.agrivam.micultivo
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.*
 import android.webkit.WebView
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         val capabilities = connectivityManager.getNetworkCapabilities(networkInfo)
         //Creamos un webview
         val webView = findViewById<WebView>(R.id.webView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
 
         var url = "https://micultivov4.000webhostapp.com"
 
@@ -30,6 +37,21 @@ class MainActivity : AppCompatActivity() {
         ) {
             // Se abre el vieweb
             webView.webViewClient = object : WebViewClient() {
+
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    // Mostrar el ProgressBar y ocultar el WebView
+                    imageView.visibility = View.VISIBLE
+                    progressBar.visibility = View.VISIBLE
+                    webView.visibility = View.INVISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    // Ocultar el ProgressBar y mostrar el WebView
+                    imageView.visibility = View.INVISIBLE
+                    progressBar.visibility = View.INVISIBLE
+                    webView.visibility = View.VISIBLE
+                }
+
                 //Si hay un error nos dirige a una plantilla para obligar a que verifique su conexion a red
                 override fun onReceivedError(
                     view: WebView?,
